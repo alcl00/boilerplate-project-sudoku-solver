4e92037878116b3f5bee6fd694d4c10f4c6f3df2
alex <aclevinson@gmail.com>
2023-08-13 14:50:02 -0400

    implemented /api/solve

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,27 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      if(!(req.body.puzzle && req.body.coordinate && req.body.value)) {
+        res.json({ error: })
+      }
       res.json({});
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-      res.json({});
+      if(!req.body.puzzle) {
+        res.json({ error: 'Required field missing' })
+      }
+      else {
+        try{
+          solver.validate(req.body.puzzle);
+          let solutionString = solver.solve(req.body.puzzle);
+          res.json({ solution: solutionString });
+        }
+        catch(err) {
+          res.json({ error: err });
+        }
+      }
+      
     });
 };
